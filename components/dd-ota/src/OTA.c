28fb44046723f6fe5dc8ec6bc96e2f1aa1a79e28
Justin Schiel <justin.schiel@ipvs.uni-stuttgart.de>
2026-08-20 15:00:44 +0200

    fixed ota time

--- components/dd-ota/src/OTA.c
+++ components/dd-ota/src/OTA.c
@@ -458,7 +458,7 @@ static int64_t utc_epoch_from_calendar(int year, int month, int day,
 
 static int64_t local_utc_offset_seconds(void)
 {
-    return 30000 * 60 * 60;
+    return 2 * 60 * 60;
 }
 
 static bool app_build_timestamp(const esp_app_desc_t *desc,
